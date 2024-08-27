380fd8e53d5b58d09c7f4945c0d670e1219140f0
AdmiN <AdmiN@ITWS0021>
2024-08-27 11:07:13 +0530

    committed

--- src/devopslab/Welcome.java
+++ src/devopslab/Welcome.java
@@ -4,7 +4,11 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to Devops Lab");
+		System.out.println("Welcome to CICD Lab");
 	}
 
+}
+public class Bye{
+	System.out.println("Welcome to Devops Lab");
+	
 }
